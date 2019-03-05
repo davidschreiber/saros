932342534292f25f0f6079200267f49a7b6cffb7
Oliver Hanßen <oliver.hanssen@gmail.com>
2019-03-05 07:56:21 +0100

    [Core] ISarosSessionManager: Renamed reference point based methods
    - addReferencePointRessourcesToSession -> addRessourcesToSession
    - startSessionWithReferencePoints -> startSession

--- core/src/saros/session/SarosSessionManager.java
+++ core/src/saros/session/SarosSessionManager.java
@@ -224,7 +224,7 @@ public class SarosSessionManager implements ISarosSessionManager {
    * point in order to start a session.)
    */
   @Override
-  public void startSessionWithReferencePoints(
+  public void startSession(
       final Map<IReferencePoint, List<IResource>> referencePointResourcesMapping) {
     /*
      * FIXME split the logic, start a session without anything and then add
@@ -523,7 +523,7 @@ public class SarosSessionManager implements ISarosSessionManager {
   }
 
   @Override
-  public synchronized void addReferencePointResourcesToSession(
+  public synchronized void addReferencePointResources(
       Map<IReferencePoint, List<IResource>> referencePointResourcesMapping) {
     if (referencePointResourcesMapping == null) {
       return;
